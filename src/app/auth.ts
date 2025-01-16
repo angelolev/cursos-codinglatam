eab7cb0371be901fee36320f138b5d6e4a85e64d
Angelo Leva <angelokta7@gmail.com>
2025-01-15 22:15:06 -0500

    feat: add user id

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,6 +1,20 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub],
+  providers: [GitHub, Google],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        // User is available during sign-in
+        token.id = user.id;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      session.user.id = token.id;
+      return session;
+    },
+  },
 });
